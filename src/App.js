ecf461112d8cc47ad7b846bca9f31b68417fc5ab
sak <seba.2312@gmail.com>
2023-07-11 14:18:37 +0200

    Updating Message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Ooty Nice to meet you :-) !
+          Test project
         </p>
         <a
           className="App-link"
